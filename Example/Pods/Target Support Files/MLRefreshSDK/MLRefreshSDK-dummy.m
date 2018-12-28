2e018e292be80741276e4a29d0dd68e1f4baa4e1
wangsanyan <valio@mem853.com>
2018-12-28 16:51:00 +0800

    first commit

---
+++ Example/Pods/Target Support Files/MLRefreshSDK/MLRefreshSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MLRefreshSDK : NSObject
+@end
+@implementation PodsDummy_MLRefreshSDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-MLRefreshSDK_Tests/Pods-MLRefreshSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MLRefreshSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MLRefreshSDK_Tests
+@end
